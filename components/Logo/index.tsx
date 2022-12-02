9cbe44d235e1100dc72311bef988429903d46b4b
Eric Echemane <eechemane29@gmail.com>
2022-12-02 17:30:46 +0800

    moved logo to logo folder

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,18 +1,19 @@
 import Image from 'next/image';
 import style from './style.module.css';
+import logoSvg from './logo.svg';
 
 type Props = {
     size: number;
 };
 
 export default function Logo({ size }: Props) {
+
     return (
         <div className={style.logo}>
             <Image
                 style={{ width: `${size * 2}rem` }}
                 width={100}
-                height={100}
-                src={'/icons/logo.svg'}
+                src={logoSvg}
                 alt='logo' />
             <div className={style.texts}>
                 <h1 style={{ fontSize: `${size}rem` }}>FMH</h1>
