d73622e81e84790efcc43ca7b6899a6817014853
Eric Echemane <eechemane29@gmail.com>
2022-12-07 12:04:04 +0800

    remove trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,7 @@
-/**
- * This is a Next.js page. 
- */
-import { CSSProperties } from 'react';
-import { trpc } from '../utils/trpc';
+import React from 'react'
 
-export default function IndexPage() {
-  // 💡 Tip: CMD+Click (or CTRL+Click) on `greeting` to go to the server definition
-  const result = trpc.getUserById.useQuery({ id: 'a8ysduyagsd' });
-
-  if (!result.data) {
-    return (
-      <div style={styles}>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+export default function HomePage() {
   return (
-    <div style={styles}>
-      {/**
-       * The type is defined and can be autocompleted
-       * 💡 Tip: Hover over `data` to see the result type
-       * 💡 Tip: CMD+Click (or CTRL+Click) on `text` to go to the server definition
-       * 💡 Tip: Secondary click on `text` and "Rename Symbol" to rename it both on the client & server
-       */}
-      <h1>{result.data.name}</h1>
-      <h1>{result.data.id}</h1>
-    </div>
-  );
+    <div>HomePage</div>
+  )
 }
-
-const styles: CSSProperties = {
-  width: '100vw',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-};
