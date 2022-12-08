2dc4554edccadac0ccec75c54828cdf27f94c8bc
Eric Echemane <eechemane29@gmail.com>
2022-12-08 19:49:09 +0800

    responsive navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Navbar from '~/components/Navbar';
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  return <>
+    <Navbar/>
+  </>
 }
