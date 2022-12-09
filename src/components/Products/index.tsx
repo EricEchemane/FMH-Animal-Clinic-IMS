fd4728ce53d06140fe2b2016f938b44035b05b37
calagkit100 <calagkit100@gmail.com>
2022-12-10 02:00:00 +0800

    UptoTestimonial

---
+++ src/components/Products/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import style from './style.module.css'
+import Image from 'next/image'
+import accessoriesImg from './assets/Accessories.svg'
+import starIcon from './assets/bxs-star.svg'
+import petMedImg from './assets/PetMedicine.svg'
+
+
+export default function Products() {
+    return (
+        <div className={style.main}>
+            <div className={style.mainWrapper}>
+                <div className={style.wrapper}>
+                    <div className={style.titleCon}>
+                        <div className={style.titleTextCon}>
+                            <h6 className={style.titleh6}>OUR PRODUCTS</h6>
+                            <h1 className={style.titleh1}><span className={style.spanText}>FMH</span> PRODUCTS</h1>
+                        </div>
+                    </div>
+                    <div className={style.productCon}>
+                        <div className={style.productCard}>
+                            <div className={style.titleAccCon}>
+                                <h2 className={style.titleh2}>ACCESSORIES</h2>
+                            </div>
+                            <div className={style.imgCon}>
+                                <Image src={accessoriesImg} alt="image" width='230' />
+                            </div>
+                            <div className={style.starCon}>
+                                <i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i>
+                            </div>
+                            <div className={style.priceText}>
+                                <h2 className={style.priceText}>150-2,000 PHP</h2>
+                            </div>
+                            <div className={style.checkAccLink}>
+                                <a href="#"><i className='bx bxs-shopping-bags'></i>CHECK ACCESSORIES<i className='bx bx-right-arrow-alt' ></i></a>
+                            </div>
+                        </div>
+                        <div className={style.productCard}>
+                            <div className={style.titleAccCon}>
+                                <h2 className={style.titleh2}>PET MEDICINES</h2>
+                            </div>
+                            <div className={style.imgCon}>
+                                <Image src={petMedImg} alt="image" width='100' />
+                            </div>
+                            <div className={style.starCon}>
+                                <i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i>
+                            </div>
+                            <div className={style.priceText}>
+                                <h2 className={style.priceText}>150-2,000 PHP</h2>
+                            </div>
+                            <div className={style.checkAccLink}>
+                                <a href="#"><i className='bx bxs-shopping-bags'></i>CHECK ACCESSORIES<i className='bx bx-right-arrow-alt' ></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,16 @@ import SampleCards from '~/components/SampleCards';
 import Hero from '~/components/Hero';
 import Navbar from '~/components/Navbar';
 import Service from '~/components/Servicess';
-
+import Products from '~/components/Products';
+import Testimonials from '~/components/Testimonials';
 
 
 export default function HomePage() {
   return <>
-    <Navbar/>
+    <Navbar />
     <Hero />
     <Service />
+    <Products />
+    <Testimonials />
   </>
 }
