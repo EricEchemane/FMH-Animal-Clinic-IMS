d6cd057b85d3ebd803cc0b2f7de30c93b62867f1
Eric Echemane <eechemane29@gmail.com>
2022-12-10 15:35:20 +0800

    hotfix/navbar: fix navbar not closing when links are clicked

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,27 +1,27 @@
-import React, { useRef } from 'react'
-import Image from 'next/image'
-import logo from './assets/logo.svg'
-import style from './style.module.css'
+import React, { useRef } from 'react';
+import Image from 'next/image';
+import logo from './assets/logo.svg';
+import style from './style.module.css';
 
 
 
 
 export default function Navbar() {
-  const navLinkRef = useRef<HTMLDivElement>(null)
-  const showRef = useRef<boolean>(false)
+  const navLinkRef = useRef<HTMLDivElement>(null);
+  const showRef = useRef<boolean>(false);
 
   const toggleNavbar = () => {
     if (!navLinkRef.current) return;
 
     if (showRef.current === false) {
-      navLinkRef.current.style.translate = '0% 0'
+      navLinkRef.current.style.translate = '0% 0';
       showRef.current = true;
     }
     else {
-      navLinkRef.current.style.translate = '100% 0'
+      navLinkRef.current.style.translate = '100% 0';
       showRef.current = false;
     }
-  }
+  };
 
   return (
 
@@ -35,15 +35,15 @@ export default function Navbar() {
         className={style.navLinks}>
         <ul>
           <i onClick={toggleNavbar} className={`bx bx-x ${style.closeIcon}`}></i>
-          <li> <a href="#" className={style.navLinkText}>Home</a> </li>
-          <li> <a href="#services">Services</a> </li>
-          <li> <a href="#products">Products</a> </li>
-          <li> <a href="#">Book Schedule</a> </li>
-          <li> <a href="#testimonials">Customer Review</a> </li>
+          <li> <a onClick={toggleNavbar} href="#" className={style.navLinkText}>Home</a> </li>
+          <li> <a onClick={toggleNavbar} href="#services">Services</a> </li>
+          <li> <a onClick={toggleNavbar} href="#products">Products</a> </li>
+          <li> <a onClick={toggleNavbar} href="#">Book Schedule</a> </li>
+          <li> <a onClick={toggleNavbar} href="#testimonials">Customer Review</a> </li>
         </ul>
       </div>
 
       <i onClick={toggleNavbar} className={`bx bx-menu ${style.hamburger}`}></i>
     </nav>
-  )
+  );
 }
