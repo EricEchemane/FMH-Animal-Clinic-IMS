0097e6e55761e940e5773a27decb75ce35e87b66
Eric Echemane <eechemane29@gmail.com>
2022-12-10 21:39:50 +0800

    sampel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import SampleCards from '~/components/SampleCards';
 import Hero from '~/components/Hero';
 import Navbar from '~/components/Navbar';
@@ -11,9 +11,9 @@ export default function HomePage() {
   return <>
     <Navbar />
     <Hero />
-    <Service />
+    {/* <Service />
     <Products />
     <Testimonials />
-    <Footer />
-  </>
+    <Footer /> */}
+  </>;
 }
