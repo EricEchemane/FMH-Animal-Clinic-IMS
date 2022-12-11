df4d0d160ccec37651b6c4c967fd4e92097064e6
Eric Echemane <eechemane29@gmail.com>
2022-12-11 08:55:55 +0800

    added home page title and meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import SampleCards from '~/components/SampleCards';
 import Hero from '~/components/Hero';
 import Navbar from '~/components/Navbar';
@@ -6,14 +6,19 @@ import Service from '~/components/Servicess';
 import Products from '~/components/Products';
 import Testimonials from '~/components/Testimonials';
 import Footer from '~/components/Footer';
+import Head from 'next/head';
 
 export default function HomePage() {
   return <>
+    <Head>
+      <title>FMH Animal Clinic</title>
+      <meta name='description' content='Your pets deserve the finest in veterinary care!' />
+    </Head>
     <Navbar />
     <Hero />
     <Service />
     <Products />
     <Testimonials />
     <Footer />
-  </>
+  </>;
 }
