e93ca43c8a152fd955a2a13217d496c5183ff3da
Eric Echemane <eechemane29@gmail.com>
2022-12-13 12:25:31 +0800

    fixed build error

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,77 +1,130 @@
 import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
-import logo from './assets/logo.svg';
+import logo from '../../../public/assets/logo.svg';
 import style from './style.module.css';
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
 import { Button } from '@mantine/core';
 
-
 export default function Navbar() {
-  const navLinkRef = useRef<HTMLDivElement>(null);
-  const showRef = useRef<boolean>(false);
-  const { data: session } = useSession();
+	const navLinkRef = useRef<HTMLDivElement>(null);
+	const showRef = useRef<boolean>(false);
+	const { data: session } = useSession();
 
-  const toggleNavbar = () => {
-    if (!navLinkRef.current || window.innerWidth > 758) return;
+	const toggleNavbar = () => {
+		if (!navLinkRef.current || window.innerWidth > 758) return;
 
-    if (showRef.current === false) {
-      navLinkRef.current.style.translate = '0% 0';
-      showRef.current = true;
-    }
-    else {
-      navLinkRef.current.style.translate = '100% 0';
-      showRef.current = false;
-    }
-  };
+		if (showRef.current === false) {
+			navLinkRef.current.style.translate = '0% 0';
+			showRef.current = true;
+		} else {
+			navLinkRef.current.style.translate = '100% 0';
+			showRef.current = false;
+		}
+	};
 
-  useEffect(() => {
-    const resizeListener = () => {
-      if (!window || !navLinkRef.current) return;
+	useEffect(() => {
+		const resizeListener = () => {
+			if (!window || !navLinkRef.current) return;
 
-      if (window.innerWidth > 758) {
-        navLinkRef.current.style.translate = '0% 0';
-        showRef.current = true;
-      }
-      else {
-        navLinkRef.current.style.translate = '100% 0';
-        showRef.current = false;
-      }
-    };
+			if (window.innerWidth > 758) {
+				navLinkRef.current.style.translate = '0% 0';
+				showRef.current = true;
+			} else {
+				navLinkRef.current.style.translate = '100% 0';
+				showRef.current = false;
+			}
+		};
 
-    window.addEventListener('resize', resizeListener);
+		window.addEventListener('resize', resizeListener);
 
-    return () => {
-      window.removeEventListener('resize', resizeListener);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener('resize', resizeListener);
+		};
+	}, []);
 
-  return (
+	return (
+		<nav className={style.nav}>
+			<div className={style.logoDiv}>
+				<Image
+					src={logo}
+					alt='logo'
+					width='180'
+				/>
+			</div>
 
-    <nav className={style.nav}>
-      <div className={style.logoDiv}>
-        <Image src={logo} alt="logo" width="180" />
-      </div>
+			<div
+				ref={navLinkRef}
+				className={style.navLinks}
+			>
+				<ul>
+					<i
+						onClick={toggleNavbar}
+						className={`bx bx-x ${style.closeIcon}`}
+					></i>
+					<li>
+						{' '}
+						<a
+							onClick={toggleNavbar}
+							href='#'
+							className={style.navLinkText}
+						>
+							Home
+						</a>{' '}
+					</li>
+					<li>
+						{' '}
+						<a
+							onClick={toggleNavbar}
+							href='#services'
+						>
+							Services
+						</a>{' '}
+					</li>
+					<li>
+						{' '}
+						<a
+							onClick={toggleNavbar}
+							href='#products'
+						>
+							Products
+						</a>{' '}
+					</li>
+					<li>
+						{' '}
+						<Link
+							onClick={toggleNavbar}
+							href='/book-schedule'
+						>
+							Book Schedule
+						</Link>{' '}
+					</li>
+					<li>
+						{' '}
+						<a
+							onClick={toggleNavbar}
+							href='#testimonials'
+						>
+							Customer Review
+						</a>{' '}
+					</li>
+					{session && session.user && (
+						<li>
+							<Button
+								onClick={() => signOut()}
+								variant='default'
+							>
+								Sign out
+							</Button>
+						</li>
+					)}
+				</ul>
+			</div>
 
-      <div
-        ref={navLinkRef}
-        className={style.navLinks}>
-        <ul>
-          <i onClick={toggleNavbar} className={`bx bx-x ${style.closeIcon}`}></i>
-          <li> <a onClick={toggleNavbar} href="#" className={style.navLinkText}>Home</a> </li>
-          <li> <a onClick={toggleNavbar} href="#services">Services</a> </li>
-          <li> <a onClick={toggleNavbar} href="#products">Products</a> </li>
-          <li> <Link onClick={toggleNavbar} href="/book-schedule">Book Schedule</Link> </li>
-          <li> <a onClick={toggleNavbar} href="#testimonials">Customer Review</a> </li>
-          {session && session.user && <li>
-            <Button onClick={() => signOut()} variant='default'> 
-              Sign out
-            </Button>
-          </li>}
-        </ul>
-      </div>
-
-      <i onClick={toggleNavbar} className={`bx bx-menu ${style.hamburger}`}></i>
-    </nav>
-  );
+			<i
+				onClick={toggleNavbar}
+				className={`bx bx-menu ${style.hamburger}`}
+			></i>
+		</nav>
+	);
 }
