9070fad58d3bbabd6de7dcf2d944ffdfed723175
Eric Echemane <eechemane29@gmail.com>
2022-12-13 12:21:12 +0800

    fixed build errors

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,66 +3,103 @@ import style from './style.module.css';
 import Image from 'next/image';
 import fmhLogo from './assets/Logo.svg';
 
-
 export default function Footer() {
-    return (
-        <div className={style.main}>
-            <div className={style.socialLinks}>
+	return (
+		<div className={style.main}>
+			<div className={style.socialLinks}>
+				<div className={style.socialLinksContainer}>
+					<div className={style.logo}>
+						<Image
+							src={fmhLogo}
+							alt='logo'
+						/>
+					</div>
 
-                <div className={style.socialLinksContainer}>
+					<div className={style.businessHours}>
+						<label htmlFor='business-hours'>
+							<h3>BUSINESS HOURS</h3>
+						</label>
+						<input
+							type='checkbox'
+							hidden
+							id='business-hours'
+						/>
+						<ul>
+							<li>8AM–6PM | Saturday</li>
+							<li>8AM–6PM | Sunday</li>
+							<li>8AM–6PM | Monday</li>
+							<li>8AM–6PM | Tuesday</li>
+							<li>8AM–6PM | Wednesday</li>
+							<li>8AM–6PM | Thursday</li>
+							<li>8AM–6PM | Friday</li>
+						</ul>
+					</div>
+					<div className={style.footerLink}>
+						<label htmlFor='pages'>
+							<h3>PAGES</h3>
+						</label>
+						<input
+							type='checkbox'
+							hidden
+							id='pages'
+						/>
+						<ul>
+							<li>
+								<a href='#'>Home</a>
+							</li>
+							<li>
+								<a href='#services'>Services</a>
+							</li>
+							<li>
+								<a href='#products'>Products</a>
+							</li>
+							<li>
+								<a href='#'>Book Schedule</a>
+							</li>
+							<li>
+								<a href='#testimonials'>Feedback</a>
+							</li>
+							<li>
+								<a href='#customer-review'>Customer Review</a>
+							</li>
+						</ul>
+					</div>
 
-                    <div className={style.logo}>
-                        <Image src={fmhLogo} alt="logo" />
-                    </div>
+					<div className={style.infoFmh}>
+						<label htmlFor='info'>
+							<h3>INFO</h3>
+						</label>
+						<input
+							type='checkbox'
+							hidden
+							id='info'
+						/>
+						<ul>
+							<li>
+								Stall E BM 7 Bldg. Molino Road Molino II, Bacoor, Cavite
+								Philippines
+							</li>
+							<li className={style.location}>
+								{/* <i className='bx bxs-location-plus' ></i> */}
+								<a
+									target={'_blank'}
+									href='https://www.google.com/maps/place/FMH+Animal+Clinic/@14.4155944,120.9722367,17z/data=!3m1!4b1!4m5!3m4!1s0x3397d23a999c7881:0x466009b758b0f4f1!8m2!3d14.4155944!4d120.9744254'
+									rel='noreferrer'
+								>
+									Check Location
+								</a>
+								<i className='bx bx-link-external'></i>
+							</li>
+							<li>(046)4772846</li>
+							<li>fmhcliinic@gmail.com</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 
-                    <div className={style.businessHours}>
-                        <label htmlFor="business-hours"><h3>BUSINESS HOURS</h3></label>
-                        <input type="checkbox" hidden id='business-hours' />
-                        <ul>
-                            <li>8AM–6PM | Saturday</li>
-                            <li>8AM–6PM | Sunday</li>
-                            <li>8AM–6PM | Monday</li>
-                            <li>8AM–6PM | Tuesday</li>
-                            <li>8AM–6PM | Wednesday</li>
-                            <li>8AM–6PM | Thursday</li>
-                            <li>8AM–6PM | Friday</li>
-                        </ul>
-                    </div>
-                    <div className={style.footerLink}>
-                        <label htmlFor="pages"><h3>PAGES</h3></label>
-                        <input type="checkbox" hidden id='pages' />
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#services">Services</a></li>
-                            <li><a href="#products">Products</a></li>
-                            <li><a href="#">Book Schedule</a></li>
-                            <li><a href="#testimonials">Feedback</a></li>
-                            <li><a href="#customer-review">Customer Review</a></li>
-                        </ul>
-                    </div>
-
-                    <div className={style.infoFmh}>
-                        <label htmlFor="info"><h3>INFO</h3></label>
-                        <input type="checkbox" hidden id='info' />
-                        <ul>
-                            <li>Stall E BM 7 Bldg. Molino Road Molino II, Bacoor, Cavite Philippines</li>
-                            <li className={style.location}>
-                                {/* <i className='bx bxs-location-plus' ></i> */}
-                                <a target={'_blank'} href="https://www.google.com/maps/place/FMH+Animal+Clinic/@14.4155944,120.9722367,17z/data=!3m1!4b1!4m5!3m4!1s0x3397d23a999c7881:0x466009b758b0f4f1!8m2!3d14.4155944!4d120.9744254">Check Location
-                                </a>
-                                <i className='bx bx-link-external'></i>
-                            </li>
-                            <li>(046)4772846</li>
-                            <li>fmhcliinic@gmail.com</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div className={style.copyRight}>
-                Copyright © 2022 FMH Animal Clinic. All rights reserved.
-            </div>
-        </div>
-    );
+			<div className={style.copyRight}>
+				Copyright © 2022 FMH Animal Clinic. All rights reserved.
+			</div>
+		</div>
+	);
 }
-
--- src/components/Servicess/index.tsx
+++ src/components/Servicess/index.tsx
@@ -1,98 +1,189 @@
-import React from 'react'
-import style from './style.module.css'
-import Image from 'next/image'
-import vaccinationImg from './assets/Vaccination.svg'
-import antiRabiesImg from './assets/Antirabies.svg'
-import surgicalImg from './assets/Surgery.svg'
-import radiologyImg from './assets/Radiology.svg'
-import diagnosticImg from './assets/Diagnostic.svg'
-import therapeuticImg from './assets/Therapeutic.svg'
-
+import React from 'react';
+import style from './style.module.css';
+import Image from 'next/image';
+import vaccinationImg from './assets/Vaccination.svg';
+import antiRabiesImg from './assets/Antirabies.svg';
+import surgicalImg from './assets/Surgery.svg';
+import radiologyImg from './assets/Radiology.svg';
+import diagnosticImg from './assets/Diagnostic.svg';
+import therapeuticImg from './assets/Therapeutic.svg';
 
 export default function Service() {
-    return (
-        <div className={style.main} id='services'>
-            <div className={style.wrapper}>
+	return (
+		<div
+			className={style.main}
+			id='services'
+		>
+			<div className={style.wrapper}>
+				<div className={style.titleCon}>
+					<div className={style.textCon}>
+						<h6 className={style.titleh6}>OUR SERVIES</h6>
+						<h1 className={style.titleh1}>
+							All Pet Care <span className={style.spanText}>Services</span>
+						</h1>
+					</div>
+				</div>
 
-                <div className={style.titleCon}>
-                    <div className={style.textCon}>
-                        <h6 className={style.titleh6}>OUR SERVIES</h6>
-                        <h1 className={style.titleh1}>All Pet Care <span className={style.spanText}>Services</span></h1>
-                    </div>
-                </div>
+				<div className={style.cardCon}>
+					<div className={style.cardConWrapper}>
+						<div className={style.cards}>
+							<div className={style.cardsTitleCon}>
+								<Image
+									src={vaccinationImg}
+									alt='logo'
+									width='50'
+								/>
+								<h1 className={style.titleVaccine}>Vaccination</h1>
+							</div>
+							<div className={style.cardsParagraph}>
+								<p>
+									{
+										'We’ll keep your pet safe from parvo, distemper, leptospirosis, rabies, corona, kennel cough and more — and send reminders when they’re due for a vaccine.'
+									}
+								</p>
+							</div>
+							<div className={style.getServiceCon}>
+								<a
+									href='#'
+									className={style.getServiceButton}
+								>
+									Get Service <i className='bx bx-right-arrow-alt'></i>
+								</a>
+							</div>
+						</div>
 
-                <div className={style.cardCon}>
-                    <div className={style.cardConWrapper}>
+						<div className={style.cards}>
+							<div className={style.cardsTitleCon}>
+								<Image
+									src={antiRabiesImg}
+									alt='logo'
+									width='50'
+								/>
+								<h1 className={style.titleVaccine}>Anti-Rabbies</h1>
+							</div>
+							<div className={style.cardsParagraph}>
+								<p>
+									We take the health and safety of your pets seriously, and
+									strongly recommend that all animals be up-to-date on their
+									vaccines, including the anti-rabies vaccine.
+								</p>
+							</div>
+							<div className={style.getServiceCon}>
+								<a
+									href='#'
+									className={style.getServiceButton}
+								>
+									Get Service <i className='bx bx-right-arrow-alt'></i>
+								</a>
+							</div>
+						</div>
 
-                        <div className={style.cards}>
-                            <div className={style.cardsTitleCon}>
-                                <Image src={vaccinationImg} alt="logo" width='50' />
-                                <h1 className={style.titleVaccine}>Vaccination</h1>
-                            </div>
-                            <div className={style.cardsParagraph}><p>We’ll keep your pet safe from parvo, distemper, leptospirosis, rabies, corona, kennel cough and more — and send reminders when they’re due for a vaccine.</p></div>
-                            <div className={style.getServiceCon}>
-                                <a href="#" className={style.getServiceButton}>Get Service <i className='bx bx-right-arrow-alt'></i></a>
-                            </div>
-                        </div>
+						<div className={style.cards}>
+							<div className={style.cardsTitleCon}>
+								<Image
+									src={surgicalImg}
+									alt='logo'
+									width='50'
+								/>
+								<h1 className={style.titleVaccine}>Surgical</h1>
+							</div>
+							<div className={style.cardsParagraph}>
+								<p>
+									We perform soft tissue surgeries like Neutering and spaying,
+									orthopaedic, and other speciality surgeries right here in our
+									clinic.
+								</p>
+							</div>
+							<div className={style.getServiceCon}>
+								<a
+									href='#'
+									className={style.getServiceButton}
+								>
+									Get Service <i className='bx bx-right-arrow-alt'></i>
+								</a>
+							</div>
+						</div>
 
-                        <div className={style.cards}>
-                            <div className={style.cardsTitleCon}>
-                                <Image src={antiRabiesImg} alt="logo" width='50' />
-                                <h1 className={style.titleVaccine}>Anti-Rabbies</h1>
-                            </div>
-                            <div className={style.cardsParagraph}><p>We take the health and safety of your pets seriously, and strongly recommend that all animals be up-to-date on their vaccines, including the anti-rabies vaccine.</p></div>
-                            <div className={style.getServiceCon}>
-                                <a href="#" className={style.getServiceButton}>Get Service <i className='bx bx-right-arrow-alt'></i></a>
-                            </div>
-                        </div>
+						<div className={style.cards}>
+							<div className={style.cardsTitleCon}>
+								<Image
+									src={radiologyImg}
+									alt='logo'
+									width='50'
+								/>
+								<h1 className={style.titleVaccine}>Radiology</h1>
+							</div>
+							<div className={style.cardsParagraph}>
+								<p>
+									{
+										"We offer advanced radiology services for pets. Our state-of-the-art equipment allows us to take high-quality X-ray images of your pet's body, providing valuable information about their health and any potential medical conditions."
+									}
+								</p>
+							</div>
+							<div className={style.getServiceCon}>
+								<a
+									href='#'
+									className={style.getServiceButton}
+								>
+									Get Service <i className='bx bx-right-arrow-alt'></i>
+								</a>
+							</div>
+						</div>
 
-                        <div className={style.cards}>
-                            <div className={style.cardsTitleCon}>
-                                <Image src={surgicalImg} alt="logo" width='50' />
-                                <h1 className={style.titleVaccine}>Surgical</h1>
-                            </div>
-                            <div className={style.cardsParagraph}><p>We perform soft tissue surgeries like Neutering and spaying, orthopaedic, and other speciality surgeries right here in our clinic.</p></div>
-                            <div className={style.getServiceCon}>
-                                <a href="#" className={style.getServiceButton}>Get Service <i className='bx bx-right-arrow-alt'></i></a>
-                            </div>
-                        </div>
+						<div className={style.cards}>
+							<div className={style.cardsTitleCon}>
+								<Image
+									src={diagnosticImg}
+									alt='logo'
+									width='50'
+								/>
+								<h1 className={style.titleVaccine}>Diagnostic</h1>
+							</div>
+							<div className={style.cardsParagraph}>
+								<p>
+									{
+										"We offer a range of therapeutic services for pets. Our team of experienced veterinarians provides specialized treatment to help manage and improve your pet's health."
+									}
+								</p>
+							</div>
+							<div className={style.getServiceCon}>
+								<a
+									href='#'
+									className={style.getServiceButton}
+								>
+									Get Service <i className='bx bx-right-arrow-alt'></i>
+								</a>
+							</div>
+						</div>
 
-                        <div className={style.cards}>
-                            <div className={style.cardsTitleCon}>
-                                <Image src={radiologyImg} alt="logo" width='50' />
-                                <h1 className={style.titleVaccine}>Radiology</h1>
-                            </div>
-                            <div className={style.cardsParagraph}><p>We offer advanced radiology services for pets. Our state-of-the-art equipment allows us to take high-quality X-ray images of your pet's body, providing valuable information about their health and any potential medical conditions.</p></div>
-                            <div className={style.getServiceCon}>
-                                <a href="#" className={style.getServiceButton}>Get Service <i className='bx bx-right-arrow-alt'></i></a>
-                            </div>
-                        </div>
-
-                        <div className={style.cards}>
-                            <div className={style.cardsTitleCon}>
-                                <Image src={diagnosticImg} alt="logo" width='50' />
-                                <h1 className={style.titleVaccine}>Diagnostic</h1>
-                            </div>
-                            <div className={style.cardsParagraph}><p>We offer a range of therapeutic services for pets. Our team of experienced veterinarians provides specialized treatment to help manage and improve your pet's health.</p></div>
-                            <div className={style.getServiceCon}>
-                                <a href="#" className={style.getServiceButton}>Get Service <i className='bx bx-right-arrow-alt'></i></a>
-                            </div>
-                        </div>
-
-                        <div className={style.cards}>
-                            <div className={style.cardsTitleCon}>
-                                <Image src={therapeuticImg} alt="logo" width='50' />
-                                <h1 className={style.titleVaccine}>Therapuetic</h1>
-                            </div>
-                            <div className={style.cardsParagraph}><p>We’ll keep your pet safe from parvo, distemper, leptospirosis, rabies, corona, kennel cough and more — and send reminders when they’re due for a vaccine.</p></div>
-                            <div className={style.getServiceCon}>
-                                <a href="#" className={style.getServiceButton}>Get Service <i className='bx bx-right-arrow-alt'></i></a>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+						<div className={style.cards}>
+							<div className={style.cardsTitleCon}>
+								<Image
+									src={therapeuticImg}
+									alt='logo'
+									width='50'
+								/>
+								<h1 className={style.titleVaccine}>Therapuetic</h1>
+							</div>
+							<div className={style.cardsParagraph}>
+								<p>
+									We’ll keep your pet safe from parvo, distemper, leptospirosis,
+									rabies, corona, kennel cough and more — and send reminders
+									when they’re due for a vaccine.
+								</p>
+							</div>
+							<div className={style.getServiceCon}>
+								<a
+									href='#'
+									className={style.getServiceButton}
+								>
+									Get Service <i className='bx bx-right-arrow-alt'></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
