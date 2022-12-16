044a5677161de511859fa7535bc4d7eff8161c72
Eric Echemane <eechemane29@gmail.com>
2022-12-16 19:02:59 +0800

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,19 +4,26 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+	static getInitialProps = getInitialProps;
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
+						rel='stylesheet'
+					/>
+					<link
+						rel='icon'
+						href='/assets/Logo.svg'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
