5fe5965ef028c7d94aca713fad2d5cf906a2e29c
calagkit100 <calagkit100@gmail.com>
2022-12-16 10:16:03 +0800

    style/services:oks na lahat

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import style from './style.module.css';
 import Image from 'next/image';
 import profilePic from './assets/Profilepic.svg';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+
+
+
 
 
 export default function Testimonials() {
@@ -13,18 +22,19 @@ export default function Testimonials() {
                         <h1 className={style.titleh1}>OUR CUSTOMER FEEDBACK</h1>
                         <h3 className={style.titleh3}>Don’t take our word for it. Trust our customers</h3>
                     </div>
-                    <div className={style.cardContainer}>
-                        <Image className={style.profilePic} src={profilePic} alt="picture" />
-                        <div className={style.userName}>
-                            <h2>James Santos</h2>
-                            <div>jamessantos@gmail.com</div>
+                    <>
+                        <div className={style.cardContainer} >
+                            <Image className={style.profilePic} src={profilePic} alt="picture" />
+                            <div className={style.userName}>
+                                <h2>James Santos</h2>
+                                <div>jamessantos@gmail.com</div>
+                            </div>
+                            <div className={style.userMessage}>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil recusandae animi saepe placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas!</p>
+                            </div>
                         </div>
-                        <div className={style.userMessage}>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil recusandae animi saepe placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas!</p>
-                        </div>
-                    </div>
+                    </>
                 </div>
-
             </div>
         </div>
     );
