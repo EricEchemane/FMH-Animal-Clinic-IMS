bb5405aea2b9aaa9b6ec7cd3ede414527ed87ecc
Eric Echemane <eechemane29@gmail.com>
2022-12-17 17:44:02 +0800

    able to display the products

--- src/components/Admin/tabs/Inventory/index.tsx
+++ src/components/Admin/tabs/Inventory/index.tsx
@@ -1,6 +1,21 @@
-import { Button, Group, Stack, Title } from '@mantine/core';
+import {
+	Button,
+	Group,
+	Stack,
+	Table,
+	Title,
+	ActionIcon,
+	Menu,
+} from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
-import { IconPlus } from '@tabler/icons';
+import {
+	IconArchive,
+	IconDots,
+	IconListDetails,
+	IconPencil,
+	IconPlus,
+	IconTrash,
+} from '@tabler/icons';
 import React, { useEffect, useState } from 'react';
 import { useUserAdmin } from '~/providers/user-admin-prodiver';
 import Http from '~/utils/http-adapter';
@@ -9,6 +24,8 @@ import { InventoryTabs } from '../../types';
 export default function Inventory() {
 	const { admin, dispatch } = useUserAdmin();
 	const [currentTab, setCurrentTab] = useState<InventoryTabs>('all');
+
+	const products = admin?.products || [];
 
 	useEffect(() => {
 		if (!admin?.access_token) return;
@@ -34,7 +51,6 @@ export default function Inventory() {
 						zIndex: 2,
 						backdropFilter: 'blur(3rem)',
 						padding: '1rem',
-						marginBottom: '2rem',
 					}}
 					position='apart'
 				>
@@ -68,7 +84,81 @@ export default function Inventory() {
 					</Group>
 				</Group>
 
-				<section hidden={currentTab !== 'all'}>All</section>
+				<section
+					style={{ paddingRight: '2rem' }}
+					hidden={currentTab !== 'all'}
+				>
+					<Title
+						weight={'normal'}
+						order={2}
+						mb={'xl'}
+					>
+						All Products
+					</Title>
+
+					<Table
+						striped
+						verticalSpacing='xl'
+						fontSize='lg'
+					>
+						<thead>
+							<tr>
+								<th>Product Name</th>
+								<th>Description</th>
+								<th>Stock</th>
+								<th>Price/Unit</th>
+								<th>Menu</th>
+							</tr>
+						</thead>
+						<tbody>
+							{products.map(({ id, name, description, stock, unit_price }) => (
+								<tr key={id}>
+									<td>{name}</td>
+									<td>{description}</td>
+									<td>{stock}</td>
+									<td>{unit_price}</td>
+									<td>
+										<Menu
+											shadow='md'
+											width={200}
+											position='left-start'
+										>
+											<Menu.Target>
+												<ActionIcon>
+													<IconDots size={30} />
+												</ActionIcon>
+											</Menu.Target>
+
+											<Menu.Dropdown>
+												<Menu.Label>{name}</Menu.Label>
+												<Menu.Item icon={<IconListDetails size={14} />}>
+													View Details
+												</Menu.Item>
+												<Menu.Item icon={<IconPencil size={14} />}>
+													Edit
+												</Menu.Item>
+
+												<Menu.Divider />
+
+												<Menu.Label>Danger zone</Menu.Label>
+												<Menu.Item icon={<IconArchive size={14} />}>
+													Archived
+												</Menu.Item>
+												<Menu.Item
+													disabled
+													color='red'
+													icon={<IconTrash size={14} />}
+												>
+													Delete permanently
+												</Menu.Item>
+											</Menu.Dropdown>
+										</Menu>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+				</section>
 				<section hidden={currentTab !== 'archive'}>Archive</section>
 				<section hidden={currentTab !== 'add'}>Add</section>
 			</Stack>
