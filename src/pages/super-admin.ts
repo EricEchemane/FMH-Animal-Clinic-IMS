68db955bcfb2046d9ef18c38067e75563407ec44
Eric Echemane <eechemane29@gmail.com>
2022-12-23 16:35:38 +0800

    prevent access of customer to super-admin route

--- src/pages/super-admin.ts
+++ src/pages/super-admin.ts
@@ -3,6 +3,7 @@ import { GetServerSideProps } from 'next';
 import SuperAdmin from '~/components/SuperAdmin';
 import jwt from 'jsonwebtoken';
 import { User } from '~/entities-interfaces/user.entity';
+import { UserRole } from '~/providers/customer-provider/types';
 
 export default SuperAdmin;
 
@@ -27,7 +28,16 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 	try {
 		const user = jwt.verify(token, process.env.JWTSECRET) as User;
 
-		if (user.role !== 'super_admin') {
+		if (user.role === UserRole.customer) {
+			return {
+				redirect: {
+					destination: '/',
+					permanent: false,
+				},
+			};
+		}
+
+		if (user.role !== UserRole.super_admin) {
 			return {
 				redirect: {
 					destination: '/sign-in',
