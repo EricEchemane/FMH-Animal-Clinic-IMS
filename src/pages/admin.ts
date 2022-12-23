8830ecd3258f1d5ea064bfc17f1ccc24f79940f9
Eric Echemane <eechemane29@gmail.com>
2022-12-23 16:33:20 +0800

    prevent access of customer to admin route

--- src/pages/admin.ts
+++ src/pages/admin.ts
@@ -2,13 +2,15 @@ import { GetServerSideProps } from 'next';
 import Cookies from 'cookies';
 import Admin from '~/components/Admin';
 import jwt from 'jsonwebtoken';
+import { User } from '~/entities-interfaces/user.entity';
+import { UserRole } from '~/providers/customer-provider/types';
 
 export default Admin;
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 	const cookies = new Cookies(req, res);
 	const token = cookies.get('token');
-	
+
 	if (!token) {
 		return {
 			redirect: {
@@ -18,23 +20,33 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 		};
 	}
 
-	if(!process.env.JWTSECRET) {
+	if (!process.env.JWTSECRET) {
 		console.log('no JWTSECRET is provided');
 		throw new Error('no JWTSECRET is provided');
 	}
 
 	try {
-		const user = jwt.verify(token, process.env.JWTSECRET);
+		const user = jwt.verify(token, process.env.JWTSECRET) as User;
+
+		if (user.role === UserRole.customer) {
+			return {
+				redirect: {
+					destination: '/',
+					permanent: false,
+				},
+			};
+		}
+
 		return {
 			props: { user },
 		};
 	} catch (error: any) {
 		console.log({ error: error?.message });
-		
+
 		return {
-				redirect: {
-					destination: '/sign-in',
-					permanent: false,
+			redirect: {
+				destination: '/sign-in',
+				permanent: false,
 			},
 		};
 	}
