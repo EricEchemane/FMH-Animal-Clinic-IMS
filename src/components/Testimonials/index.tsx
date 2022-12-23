21796b32a164459ab390da72be46e77f9ed2760a
calagkit100 <calagkit100@gmail.com>
2022-12-23 15:16:58 +0800

    Change/FeedbackCards: feedback cards has been changed ang added

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import style from './style.module.css';
 import Image from 'next/image';
 import profilePic from './assets/Profilepic.svg';
+import profilePic1 from './assets/ImageProfilePic-1.svg'
+import profilePic2 from './assets/ImageProfilePic-2.svg'
+import profilePic3 from './assets/ImageProfilePic-3.svg'
+import { Rating, Center } from '@mantine/core';
 
 
 
@@ -11,25 +15,74 @@ export default function Testimonials() {
     return (
         <div className={style.main} id='testimonials'>
             <div className={style.mainWrapper}>
+
                 <div className={style.testimonialContainer}>
+
                     <div className={style.titleContainer}>
                         <h1 className={style.titleh1}>OUR CUSTOMER FEEDBACK</h1>
                         <h3 className={style.titleh3}>Don’t take our word for it. Trust our customers</h3>
                     </div>
-                    <>
-                        <div className={style.cardContainer} >
-                            <Image className={style.profilePic} src={profilePic} alt="picture" />
+                    <div className={style.cardsContainer}>
+
+                        <div className={style.cardsthis}>
+                            <Image className={style.profilePic} src={profilePic1} alt="picture" />
+                            <Center>
+                                <Rating value={4} readOnly />
+                            </Center>
                             <div className={style.userName}>
-                                <h2>James Santos</h2>
-                                <div>jamessantos@gmail.com</div>
+                                <h2>Tony Darien</h2>
+                                <div><h4>tonydarien@gmail.com</h4></div>
                             </div>
                             <div className={style.userMessage}>
                                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil recusandae animi saepe placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas!</p>
                             </div>
                         </div>
-                    </>
+
+                        <div className={style.cardsthis}>
+                            <Image className={style.profilePic} src={profilePic2} alt="picture" />
+                            <Center>
+                                <Rating value={4} readOnly />
+                            </Center>
+                            <div className={style.userName}>
+                                <h2>Nora Kirrily</h2>
+                                <div><h4>norakirrilyn@gmail.com</h4></div>
+                            </div>
+                            <div className={style.userMessage}>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil recusandae animi saepe placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas!</p>
+                            </div>
+                        </div>
+
+                        <div className={style.cardsthis}>
+                            <Image className={style.profilePic} src={profilePic3} alt="picture" />
+                            <Center>
+                                <Rating value={4} readOnly />
+                            </Center>
+                            <div className={style.userName}>
+                                <h2>Tucker Soren</h2>
+                                <div><h4>tuckesoren@gmail.com</h4></div>
+                            </div>
+                            <div className={style.userMessage}>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil recusandae animi saepe placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas!</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
                 </div>
             </div>
         </div>
     );
 }
+
+
+{/* <div className={style.cardContainer} >
+    <Image className={style.profilePic} src={profilePic} alt="picture" />
+    <div className={style.userName}>
+        <h2>James Santos</h2>
+        <div><h4>jamessantos@gmail.com</h4></div>
+    </div>
+    <div className={style.userMessage}>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil recusandae animi saepe placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas placeat temporibus culpa a sunt quas consequuntur sint. Odio totam ipsam vel voluptas!</p>
+    </div>
+</div> */}
