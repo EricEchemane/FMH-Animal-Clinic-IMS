879d199b4d0a07feb933015627a939193de25b81
Eric Echemane <eechemane29@gmail.com>
2022-12-25 20:13:54 +0800

    new server

--- src/constants/backend.ts
+++ src/constants/backend.ts
@@ -1,2 +1,4 @@
-export const backendUrl = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:2900' : 'https://fmh-clinic-backend-production.up.railway.app';
+export const backendUrl =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:2900'
+		: 'https://fmh.onrender.com';
