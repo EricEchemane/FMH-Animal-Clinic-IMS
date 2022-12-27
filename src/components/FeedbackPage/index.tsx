8cbc2c9e04ce96fd9438b24baa7295390548131a
Eric Echemane <eechemane29@gmail.com>
2022-12-27 10:44:58 +0800

    redesign write feedback

--- src/components/FeedbackPage/index.tsx
+++ src/components/FeedbackPage/index.tsx
@@ -1,66 +1,189 @@
-import React from 'react'
-import css from './style.module.css'
-import { Button, Group } from '@mantine/core';
-import Link from 'next/link'
-
+import React, { useEffect } from 'react';
+import {
+	Button,
+	Card,
+	Group,
+	List,
+	Rating,
+	ThemeIcon,
+	Title,
+	Text,
+	Stack,
+	SimpleGrid,
+	TextInput,
+	Textarea,
+} from '@mantine/core';
+import { useSession } from 'next-auth/react';
+import Router from 'next/router';
+import { useForm } from '@mantine/form';
+import Head from 'next/head';
+import { IconPaw } from '@tabler/icons';
+import BACKG from './assets/BACKG.svg';
 
 export default function Feedback() {
+	const { data: session } = useSession({
+		required: true,
+		onUnauthenticated() {
+			Router.replace('/');
+		},
+	});
+
+	const feedbackForm = useForm({
+		initialValues: {
+			name: '',
+			email: '',
+			message: '',
+			rating: 0,
+		},
+	});
+
+	useEffect(() => {
+		if (session && session.user) {
+			feedbackForm.setValues({
+				name: session.user.name!,
+				email: session.user.email!,
+			});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [session]);
+
 	return (
-		<div className={css.main}>
-			<div className={css.Container}>
-				<div className={css.titleContainer}><h1>Write a Feedback</h1></div>
-				<div className={css.formContainer}>
-					<div className={css.forms}>
-						<div className={css.labelNameCon}>
-							<label htmlFor="Name">Name</label>
-							<input required className={css.userName} type="text" id='name' name='name' placeholder='Your Name' />
-						</div>
-						<div className={css.labelEmailCon}>
-							<label htmlFor="Email">Email</label>
-							<input className={css.userEmail} type="email" id='email' placeholder='Your Email' />
-						</div>
-						<div className={css.labelMessageCon}>
-							<label htmlFor="Message">Write a feedback</label>
-							<textarea className={css.textArea} name="message" id="message" cols={30} rows={10} placeholder='Message'></textarea>
-						</div>
-						{/* <div className={css.buttonContainer}> */}
-						<Group>
-							<Button color="yellow" size="md">
-								Submit
+		<>
+			<Head>
+				<title> Write Feedback </title>
+			</Head>
+
+			<SimpleGrid
+				cols={2}
+				m='auto'
+				my={'xl'}
+				spacing='xl'
+				sx={{ maxWidth: '1000px' }}
+			>
+				<form>
+					<Stack mt={'xl'}>
+						<Stack>
+							<Title order={2}>How was your experience with our clinic?</Title>
+							<label htmlFor='rating'>Your rating</label>
+							<Rating
+								{...feedbackForm.getInputProps('rating')}
+								name='rating'
+							/>
+						</Stack>
+						<TextInput
+							label='Your name'
+							{...feedbackForm.getInputProps('name')}
+							required
+							readOnly
+							size='lg'
+						/>
+						<TextInput
+							label='Your email'
+							{...feedbackForm.getInputProps('email')}
+							required
+							readOnly
+							size='lg'
+						/>
+						<Textarea
+							label='Your comment'
+							placeholder='Your comment'
+							{...feedbackForm.getInputProps('message')}
+							required
+							readOnly
+							size='lg'
+							minRows={5}
+						/>
+						<Group mt='md'>
+							<Button
+								size='md'
+								color={'orange'}
+							>
+								Submit review
 							</Button>
-							<Link href="/" passHref>
-								<Button variant='outline' size="md" color="yellow">
-									Cancel
-								</Button>
-							</Link>
+							<Button
+								size='md'
+								color={'orange'}
+								variant='outline'
+								onClick={() => Router.back()}
+							>
+								Cancel
+							</Button>
 						</Group>
-						{/* </div> */}
-					</div>
-					<div className={css.instructions}>
-						<h1>How to write review</h1>
-						<ul>
-							<li>
-								{"Think about your overall experience with the clinic. Did you have a positive or negative experience? What stood out to you?"}
-							</li>
-							<li>
-								{"Be specific about what you liked or didn't like. Did the staff treat you and your pet well? Was the clinic clean and well-maintained? Did the veterinarian take the time to explain things to you?"}
-							</li>
-							<li>
-								{"Keep it brief. A few sentences or a short paragraph should be sufficient."}
-							</li>
-							<li>
-								{"Be honest and respectful. Don't leave a fake review or use offensive language."}
-							</li>
-							<li>
-								{"Consider the tone of your review. You don't want to sound angry or bitter, even if you had a negative experience. Try to be constructive and offer suggestions for improvement."}
-							</li>
-							<li>
-								{"Proofread your review before submitting it. Make sure it's clear and easy to understand."}
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+					</Stack>
+				</form>
+
+				<Card
+					withBorder
+					radius={'lg'}
+					shadow={'md'}
+					p='xl'
+				>
+					<Title
+						mb={'lg'}
+						order={2}
+						color='dimmed'
+					>
+						How to write review
+					</Title>
+					<List
+						spacing='xs'
+						size='sm'
+						center
+						icon={
+							<ThemeIcon
+								color='teal'
+								size={24}
+								radius='xl'
+							>
+								<IconPaw />
+							</ThemeIcon>
+						}
+					>
+						<List.Item>
+							<Text size={'md'}>
+								{
+									'Think about your overall experience with the clinic. Did you have a positive or negative experience? What stood out to you?'
+								}
+							</Text>
+						</List.Item>
+						<List.Item>
+							<Text size={'md'}>
+								{
+									"Be specific about what you liked or didn't like. Did the staff treat you and your pet well? Was the clinic clean and well-maintained? Did the veterinarian take the time to explain things to you?"
+								}
+							</Text>
+						</List.Item>
+						<List.Item>
+							<Text size={'md'}>
+								{
+									'Keep it brief. A few sentences or a short paragraph should be sufficient.'
+								}
+							</Text>
+						</List.Item>
+						<List.Item>
+							<Text size={'md'}>
+								{
+									"Be honest and respectful. Don't leave a fake review or use offensive language."
+								}
+							</Text>
+						</List.Item>
+						<List.Item>
+							<Text size={'md'}>
+								{
+									"Consider the tone of your review. You don't want to sound angry or bitter, even if you had a negative experience. Try to be constructive and offer suggestions for improvement."
+								}
+							</Text>
+						</List.Item>
+						<List.Item>
+							<Text size={'md'}>
+								{
+									"Proofread your review before submitting it. Make sure it's clear and easy to understand."
+								}
+							</Text>
+						</List.Item>
+					</List>
+				</Card>
+			</SimpleGrid>
+		</>
+	);
 }
