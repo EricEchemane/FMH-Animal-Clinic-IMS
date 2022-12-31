27d8d6e76b8a29805d26baa1b98ae5d8ba235394
Eric Echemane <eechemane29@gmail.com>
2022-12-31 12:51:01 +0800

    created messenger bot link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import Service from '~/components/Servicess';
 import Testimonials from '~/components/Testimonials';
 import Footer from '~/components/Footer';
 import Head from 'next/head';
+import Link from 'next/link';
+import { Button } from '@mantine/core';
+import { IconBrandMessenger } from '@tabler/icons';
 
 export default function HomePage() {
 	return (
@@ -22,6 +25,20 @@ export default function HomePage() {
 			{/* <Products /> */}
 			<Testimonials />
 			<Footer />
+
+			<a
+				id='open-chat-bot'
+				target={'_blank'}
+				rel='noreferrer'
+				href={'https://www.messenger.com/t/107034002274525/'}
+			>
+				<Button
+					leftIcon={<IconBrandMessenger />}
+					radius={'xl'}
+				>
+					Message us
+				</Button>
+			</a>
 		</>
 	);
 }
