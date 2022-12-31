23a1c53017175e0f9be10c46c5eb6e50372675c7
calagkit100 <calagkit100@gmail.com>
2022-12-31 12:28:13 +0800

    AboutUS/Services: Service card hover fixed, About us layout almost done

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -13,7 +13,7 @@ export const AboutUs = () => {
             <div className={css.mainWrapper}>
                 <div className={css.textContainer}>
                     <h1>ABOUT US</h1>
-                    <div>
+                    <div className={css.paragraphContainer}>
                         <p>Welcome to our pet clinic! We are a team of passionate and dedicated veterinarians, technicians, and support staff who are committed to providing the highest quality medical care for your furry friends.</p>
                         <p>Our team is dedicated to maintaining a warm and welcoming environment for both pets and their owners, and we are always happy to answer any questions or concerns you may have.</p>
                         <p> We are grateful for the trust that our clients place in us and we look forward to serving you and your beloved pets for many years to come. Thank you for choosing us as your trusted pet care provider.</p>
@@ -23,28 +23,40 @@ export const AboutUs = () => {
 
                 <div className={css.infoContainer}>
                     <Image src={AboutUsImg} alt="image" />
-                    <div>
-                        <Image src={AdressIcon} alt="icon" />
-                        <h3>Address</h3>
-                        <a
-                            target={'_blank'}
-                            href='https://www.google.com/maps/dir/14.4081327,121.0414667/11+Ruby+Rd,+Las+Pi%C3%B1as,+Metro+Manila/@14.4220543,121.008966,16.66z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3397d1ea3c1a3835:0xa2a4607a02aebc1b!2m2!1d121.0088434!2d14.424671'
-                            rel='noreferrer'
-                        >
-                            11 Ruby Rd, Las Piñas, Metro Manila Philippines
-                        </a>
-                        <i className='bx bx-link-external'></i>
+
+
+                    <div className={css.addressContainer}>
+                        <Image src={AdressIcon} alt="icon" width={45} />
+                        <div className={css.linkContainer}>
+                            <h3>Address</h3>
+                            <a
+                                target={'_blank'}
+                                href='https://www.google.com/maps/dir/14.4081327,121.0414667/11+Ruby+Rd,+Las+Pi%C3%B1as,+Metro+Manila/@14.4220543,121.008966,16.66z/data=!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3397d1ea3c1a3835:0xa2a4607a02aebc1b!2m2!1d121.0088434!2d14.424671'
+                                rel='noreferrer'
+                            >
+                                11 Ruby Rd, Las Piñas, Metro Manila Philippines<i className='bx bx-link-external'></i>
+                            </a>
+
+                        </div>
+
                     </div>
-                    <div>
+
+                    <div className={css.phoneContainer}>
                         <Image src={PhoneIcon} alt="icon" />
-                        <h3>Phone</h3>
-                        <p>(02)88065772</p>
+                        <div>
+                            <h3>Phone</h3>
+                            <p>(02)88065772</p>
+                        </div>
                     </div>
-                    <div>
+
+                    <div className={css.emailContainer}>
                         <Image src={EmailIcon} alt="icon" />
-                        <h3>Email</h3>
-                        <p>fmhanimalclinic@gmail.com</p>
+                        <div>
+                            <h3>Email</h3>
+                            <p>fmhanimalclinic@gmail.com</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
